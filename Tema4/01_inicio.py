a702cba613cb87b9cead7e63d9d868ea6b6fb32b
Develop and System <developandsystem@gmail.com>
2023-03-05 14:54:00 +0100

    update

---
+++ Tema4/01_inicio.py
@@ -0,0 +1,18 @@
+# comentario linea simple
+
+"""Comentario
+que se puede escribir en varias líneas
+para explciar algo un poco más largo
+"""
+
+nombre = "borja"
+print("Imprimir por consola algo")
+print(nombre)
+print(type(10))
+arrayDatos = ["Array de datos", "Para poder evualuar", "Varias posiciones"]
+elemento = {
+    'nombre': "Borja",
+    'apellido': "Martín",
+    'clases': ["PMDM", "AD", "SGE"]
+}
+print(elemento.nombre)
